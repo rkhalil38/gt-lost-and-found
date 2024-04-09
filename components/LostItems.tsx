171e837111d7a82ea645ba39f2e27dd93429d9eb
rkhalil38 <romuluskhalil3@gmail.com>
2024-04-09 12:27:42 -0400

    added suspense boundary

--- components/LostItems.tsx
+++ components/LostItems.tsx
@@ -1,6 +1,6 @@
 "use client";
 import { Database } from "@/supabase";
-import React, { useEffect, useState } from "react";
+import React, { Suspense, useEffect, useState } from "react";
 import { reactIconMatcher } from "@/utils/supabase/iconMatcher";
 import Link from "next/link";
 import Skeleton from "react-loading-skeleton";
@@ -58,49 +58,51 @@ const LostItems = () => {
   }
 
   return (
-    <div className="flex items-start justify-start pt-44 pb-24 pb:py-28 px-10 w-screen min-h-screen">
-      <div className={`flex flex-col pt-0 pb:pt-24 tb:pt-0 px-0 pb:px-14 tb:px-0 pb:flex-row pb:flex-wrap items-center pb:items-start gap-6 w-full`}>
-        {loading ? (
-          <div className="flex flex-col border-[1px] border-gray-500 cursor-pointer duration-500 gap-4 p-2 items-center w-72 h-fit pb:h-96 bg-mainHover rounded-lg">
-            <div className="flex flex-col gap-4 p-4 border-[1px] border-gray-500 group-hover:border-white items-center rounded-lg duration-300 justify-center w-full h-1/2">
-              <div className="hidden pb:block"><Skeleton width={100} height={100} baseColor="#B3A369" /></div>
-              <h1 className="font-semibold text-white">
-                <Skeleton width={90} baseColor="#B3A369" count={1} />
-              </h1>
-            </div>
-            <div className="flex flex-col justify-center items-center rounded-lg w-full h-[40%]">
-              <p className="text-gtGold">
-                <Skeleton width={90} baseColor="#B3A369" />
+    <Suspense>
+      <div className="flex items-start justify-start pt-44 pb-24 pb:py-28 px-10 w-screen min-h-screen">
+        <div className={`flex flex-col pt-0 pb:pt-24 tb:pt-0 px-0 pb:px-14 tb:px-0 pb:flex-row pb:flex-wrap items-center pb:items-start gap-6 w-full`}>
+          {loading ? (
+            <div className="flex flex-col border-[1px] border-gray-500 cursor-pointer duration-500 gap-4 p-2 items-center w-72 h-fit pb:h-96 bg-mainHover rounded-lg">
+              <div className="flex flex-col gap-4 p-4 border-[1px] border-gray-500 group-hover:border-white items-center rounded-lg duration-300 justify-center w-full h-1/2">
+                <div className="hidden pb:block"><Skeleton width={100} height={100} baseColor="#B3A369" /></div>
+                <h1 className="font-semibold text-white">
+                  <Skeleton width={90} baseColor="#B3A369" count={1} />
+                </h1>
+              </div>
+              <div className="flex flex-col justify-center items-center rounded-lg w-full h-[40%]">
+                <p className="text-gtGold">
+                  <Skeleton width={90} baseColor="#B3A369" />
+                </p>
+              </div>
+              <p className="text-gray-500 text-xs">
+                <Skeleton width={150} baseColor="#B3A369" />
               </p>
             </div>
-            <p className="text-gray-500 text-xs">
-              <Skeleton width={150} baseColor="#B3A369" />
-            </p>
-          </div>
-        ) : (
-            pins.map((pin) => (
-              <Link
-                href={`/lostitems/${pin.item_id}`}
-                key={pin.created_at}
-                className={`${filterElement(filter, pin)? 'flex' : 'hidden'} flex-col overflow-clip group hover:bg-mainHover2 border-[1px] 
-                border-gray-500 cursor-pointer duration-300 gap-4 p-2 items-center w-72 h-fit pb:h-96 bg-mainHover shadow-lg rounded-lg`}
-              >
-                <div className="flex flex-col p-4 border-[1px] border-gray-500 tb:group-hover:border-white items-center rounded-lg duration-300 justify-center w-full h-1/2">
-                  <div className="hidden pb:block">{reactIconMatcher[pin.item ? pin.item : "miscellaneous"]}</div>
-                  <h1 className="font-semibold text-white">{pin.item}</h1>
-                </div>
-                <div className="flex flex-col justify-center items-center rounded-lg w-full h-[40%]">
-                  <p className="text-gtGold duration-300">{pin.description}</p>
-                </div>
-                <p className="text-gray-500 tb:group-hover:text-white duration-300 text-xs">
-                  Found by {pin.user_name} on {pin.created_at.slice(0, 10)}
-                </p>
-              </Link>
-            ))
-        )}
+          ) : (
+              pins.map((pin) => (
+                <Link
+                  href={`/lostitems/${pin.item_id}`}
+                  key={pin.created_at}
+                  className={`${filterElement(filter, pin)? 'flex' : 'hidden'} flex-col overflow-clip group hover:bg-mainHover2 border-[1px] 
+                  border-gray-500 cursor-pointer duration-300 gap-4 p-2 items-center w-72 h-fit pb:h-96 bg-mainHover shadow-lg rounded-lg`}
+                >
+                  <div className="flex flex-col p-4 border-[1px] border-gray-500 tb:group-hover:border-white items-center rounded-lg duration-300 justify-center w-full h-1/2">
+                    <div className="hidden pb:block">{reactIconMatcher[pin.item ? pin.item : "miscellaneous"]}</div>
+                    <h1 className="font-semibold text-white">{pin.item}</h1>
+                  </div>
+                  <div className="flex flex-col justify-center items-center rounded-lg w-full h-[40%]">
+                    <p className="text-gtGold duration-300">{pin.description}</p>
+                  </div>
+                  <p className="text-gray-500 tb:group-hover:text-white duration-300 text-xs">
+                    Found by {pin.user_name} on {pin.created_at.slice(0, 10)}
+                  </p>
+                </Link>
+              ))
+          )}
+        </div>
+        <FilterComponent filter={filter}/>
       </div>
-      <FilterComponent filter={filter}/>
-    </div>
+    </Suspense>
   );
 };
 
