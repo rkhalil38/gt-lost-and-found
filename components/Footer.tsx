0ad802d7d16ee95495ac9130073f114501280299
rkhalil38 <romuluskhalil3@gmail.com>
2024-04-12 17:27:08 -0400

    added link to github

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,12 +1,22 @@
+import Link from "next/link";
 import React from "react";
+import { FaGithub } from "react-icons/fa";
 
 const Footer = () => {
   return (
-    <div>
-        <footer className="flex flex-col text-gtGold w-screen fixed bottom-0 left-0 border-t-[1px] border-gray-500 items-center justify-center bg-mainHover text-xs h-12">
-            Copyright © by Romulus Khalil
-        </footer> 
-    </div>
+    <footer className="flex flex-row text-gtGold w-screen fixed bottom-0 left-0 border-t-[1px] border-gray-500 items-center justify-between bg-mainHover text-xs h-12">
+      <Link
+        className="flex items-center justify-center duration-300 gap-2 w-12 pb:w-28 h-full hover:bg-mainHover2"
+        href={"https://github.com/rkhalil38/gt-lost-and-found"}
+      >
+        <FaGithub className="text-2xl"/>
+        <p className="text-xs hidden pb:block">Github</p>
+      </Link>
+      <div className="flex flex-grow items-center justify-center">
+        <p>Copyright © by Romulus Khalil</p>
+      </div>
+      <div className="w-12 pb:w-28"/>
+    </footer>
   );
 };
 
